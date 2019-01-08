ebab368e6c1a0cced2e986f12e3fda66fea393d5
Houmar Passareli <houmarpassarelli@gmail.com>
2019-01-08 01:01:40 -0300

    Manipulação do banco de dados

--- migrations.js
+++ migrations.js
@@ -26,14 +26,14 @@ const createTables = () => {
 
     const queryGen = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     CREATE OR REPLACE FUNCTION trigger_set_timestamp()
-    RETURNS TRIGGER AS $$
+        RETURNS TRIGGER AS $$
     BEGIN
-    NEW.updated_at = NOW();
-    RETURN NEW;
+        NEW.updated_at = NOW();
+        RETURN NEW;
     END;
     $$ LANGUAGE plpgsql;
     CREATE TABLE IF NOT EXISTS cupom(
-        id_cupom UUID primary key not null default uuid_generate_v4(),
+        id_cupom SERIAL primary key,
         id_estabelecimento integer null,
         id_segmento integer null,
         id_pacote integer null,
@@ -47,13 +47,36 @@ const createTables = () => {
         validade timestamptz null,
         created_at timestamptz not null default now(),
         updated_at timestamptz 
-        );
-        CREATE TRIGGER set_timestamp
+    );
+    CREATE TRIGGER set_timestamp
         BEFORE UPDATE ON cupom
         FOR EACH ROW
-        EXECUTE PROCEDURE trigger_set_timestamp();`;
-        
-        
+    EXECUTE PROCEDURE trigger_set_timestamp();
+    CREATE TABLE IF NOT EXISTS usuario(
+        id_usuario SERIAL primary key,
+        codigo text null,
+        nome varchar(300) null,
+        sobrenome varchar(300) null,
+        tipo char(1) null,
+        email varchar(400) null,
+        visivel char(1) default 'S',
+        created_at timestamp not null default now(),
+        updated_at timestamp
+    );
+    CREATE TRIGGER set_timestamp
+        BEFORE UPDATE ON usuario
+        FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
+    CREATE TABLE IF NOT EXISTS tag(
+        id_tag SERIAL primary key,
+        titulo varchar(300) null,
+        created_at timestamp not null default now(),
+        updated_at timestamp timestamp
+    );
+    CREATE TRIGGER set_timestamp
+        BEFORE UPDATE ON tag
+        FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();`;
         
         
         pool.query(queryGen).then((res) => {
